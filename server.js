4f92e0853cdccdfe5ad045423e76fca51ba1b4f9
ayuk007 <ayush.nashine4807@gmail.com>
2023-11-24 10:45:04 +0530

    Update: Folder hierarchy functionality added

--- server.js
+++ server.js
@@ -1,34 +1,55 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
-const app = express();
-const port = 5000;
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const fs = require("fs");
 
-app.use(cors());
+var app = express();
+app.use(cors()); // Allows incoming requests from any IP
+app.use(express.urlencoded({ extended: false }));
+
+// Start by creating some disk storage options:
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const relativePath = path.join('uploads', path.dirname(file.originalname));
-        const fullPath = path.join(__dirname, relativePath);
+destination: function (req, file, callback) {
+    const projectName = req.body.projectName;
+    const fileType = req.body.fileType;
+    const uploadPath = `${__dirname}/uploads/${projectName}`;
+    const imagePath = uploadPath + "/image";
+    const labelPath = uploadPath + "/label";
 
-        if (!fs.existsSync(fullPath)){
-            fs.mkdirSync(fullPath, { recursive: true });
-        }
-
-        cb(null, relativePath);
-    },
-    filename: (req, file, cb) => {
-        cb(null, path.basename(file.originalname));
+    // Check if the "uploads" folder exists, if not, create it
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
+      fs.mkdirSync(imagePath);
+      fs.mkdirSync(labelPath);
     }
+    if(fileType === "img") {
+      callback(null, imagePath);
+    } else {
+      callback(null, labelPath);
+    }
+    console.log(req.body, "hleoe");
+    // callback(null, uploadPath);
+  },
+  // Sets file(s) to be saved in uploads folder in same directory
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+  // Sets saved filename(s) to be original filename(s)
 });
 
+// Set saved storage options:
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.array('files[]'), (req, res) => {
-    res.json({message: 'Folder uploaded successfully!'});
+app.post("/upload", upload.array("files"), (req, res) => {
+  // Sets multer to intercept files named "files" on uploaded form data
+
+  console.log(req.body.projectName); // Logs form body values
+  console.log(req.files); // Logs any files
+  res.json({ message: "File(s) uploaded successfully" });
+  
+
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(5000, function () {
+  console.log("Server running on port 5000");
 });
